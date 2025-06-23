29fc011d7b339a5e1796ce906af3051744965637
Your Name <atharvakailaskadam1809@gmail.com>
2025-06-23 23:14:47 +0530

    Done and Dusted

---
+++ Java Codes from Basic to Advanced/C1_HelloWorld.java
@@ -0,0 +1,13 @@
+public class C1_HelloWorld
+{
+    public void HellWrld()
+    {
+        System.out.println("Hello World....!");
+    }
+    // Main method to run the program
+    public static void main(String[] args)
+    {
+        C1_HelloWorld HW = new C1_HelloWorld();
+        HW.HellWrld();
+    }
+}

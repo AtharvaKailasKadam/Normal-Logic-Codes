bafd94c3486e9122bcfcd61cdc343dfdd64ea695
Your Name <atharvakailaskadam1809@gmail.com>
2025-07-04 23:04:10 +0530

    Done and Dusted

---
+++ Java Codes from Basic to Advanced/C21_Max_and_Min_in_Array.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+class C21_Max_and_Min_in_Array
+{
+    public void Array(int a[])
+    {
+        int Max;
+        int Min;
+        for(int i = 0; i < a.length; i++)
+        {
+            if(a[i+1] > a[i])
+            {
+                Max = a[i+1];
+                System.out.println("The Maximum Number in the Array is : "+ Max);
+            }
+            else if(a[i+1]<a[1])
+            {
+                Min = a[i+1];
+                System.out.println("The Minimum Number in the Array is : "+ Min);
+            }
+        }
+    }
+
+    public static void main(String[] args)
+    {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the Size of the Array : ");
+        int size = scanner.nextInt();
+
+        System.out.println("Enter the Numbers(Elements) of the Array : ");
+        for(int i = 0; i < size; i++)
+        {
+            i = scanner.nextInt();
+        }
+
+        scanner.close();
+    }
+}

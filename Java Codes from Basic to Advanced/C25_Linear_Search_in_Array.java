1ddfac4db68209b350ba050b0e505a781cc07df3
Your Name <atharvakailaskadam1809@gmail.com>
2025-08-02 13:07:59 +0530

    Done and Dusted

---
+++ Java Codes from Basic to Advanced/C25_Linear_Search_in_Array.java
@@ -0,0 +1,58 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class C25_Linear_Search_in_Array
+{
+    public void Liner_Search(int[] arr, int key)
+    {
+        System.out.println("The Elements in the Array are :"+Arrays.toString(arr));
+        boolean flag = false ;
+        int index = 0;
+        for(int i = 0; i < arr.length; i++)
+        {
+            if(arr[i] == key)
+            {
+                flag = true;
+                index = i;
+                break;
+            }
+            else
+            {
+                flag = false;
+            }
+        }
+
+        if(flag == true)
+        {
+            System.out.println("Key Element found on the Index : "+index);
+        }
+        else
+        {
+            System.out.println("Key Element not Found...!");
+        }
+    }
+
+    public static void main(String args[])
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the size of the Array : ");
+        int size = scanner.nextInt();
+
+        System.out.println(("Enter the Elements in the Array : "));
+        int[] Array = new int[size];
+
+        for(int i = 0; i < size; i++)
+        {
+            Array[i] = scanner.nextInt();
+        }
+
+        System.out.println("Enter the 'Element' to be Searched : ");
+        int keyElement = scanner.nextInt();
+
+        scanner.close();
+
+        C25_Linear_Search_in_Array LSA = new C25_Linear_Search_in_Array();
+        LSA.Liner_Search(Array, keyElement);
+    }
+}

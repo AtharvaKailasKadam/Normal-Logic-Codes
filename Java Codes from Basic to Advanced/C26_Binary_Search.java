3642d031c3b8351d3c44a47f2eda90680a919f7a
Your Name <atharvakailaskadam1809@gmail.com>
2025-08-16 21:57:06 +0530

    Done and Dusted

--- Java Codes from Basic to Advanced/C26_Binary_Search.java
+++ Java Codes from Basic to Advanced/C26_Binary_Search.java
@@ -1,15 +1,66 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class C26_Binary_Search
 {
     public void Binary_Search(int arr[], int n, int target)
     {
-        System.out.println("Binary Search");
+        int low = 0;
+        int high = n-1;
+        boolean Found = false;
+        System.out.println("The Elements in the Array are : "+Arrays.toString(arr));
+        for(int i = 0; i < n; i++)
+        {
+            int mid = (low + high) / 2;
+            if(arr[mid] == target)
+            {
+                Found = true;
+                System.out.println("Key Element Found at Index : " +mid);
+                break;
+            }
+            else if (arr[mid] < target)
+            {
+                low = mid + 1;
+            }
+            else if (arr[mid] > target)
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                Found = false;
+                System.out.println("Key Element Not Found....!");
+            }
+        }
+        if(Found == false)
+        {
+            System.out.println("Key Element Not Found in the Array...!");
+        }
     }
 
     public static void main(String[] args)
     {
         System.out.println("Binary Search Program");
-        System.out.println("Enter the Size of the Array");
+        System.out.println("Enter the Size of the Array : ");
+
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+
+        System.out.println("Enter the Elements in the Array : ");
+        int[] Array = new int[size];
+
+        for(int i = 0; i < size; i++)
+        {
+            Array[i] = scanner.nextInt();
+        }
+
+        System.out.println("Enter the Element to be Searched : ");
+        int keyElement = scanner.nextInt();
+
+        scanner.close();
+
+        C26_Binary_Search BS = new C26_Binary_Search();
+        BS.Binary_Search(Array, size, keyElement);
+
     }
 }

caa1a858d14f44b063fc16467554498c293349a8
Your Name <atharvakailaskadam1809@gmail.com>
2025-08-21 16:21:48 +0530

    Done

--- Java Codes from Basic to Advanced/C31_Class_(Fields_and_Methods).java
+++ Java Codes from Basic to Advanced/C31_Class_(Fields_and_Methods).java
@@ -1,4 +1,7 @@
-public class C31_Class_Fields_and_Methods
+public class C31_Class_(Fields_and_Methods)
 {
-    
+    public void printHello()
+    {
+        System.out.println("Hello World!");
+    }
 }

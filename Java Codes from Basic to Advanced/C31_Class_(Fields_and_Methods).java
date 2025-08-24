a9c882e9e93e22d118ed7c7a64683ab14d1c26b8
Your Name <atharvakailaskadam1809@gmail.com>
2025-08-24 09:48:35 +0530

    Done

--- Java Codes from Basic to Advanced/C31_Class_(Fields_and_Methods).java
+++
@@ -1,7 +0,0 @@
-public class C31_Class_(Fields_and_Methods)
-{
-    public void printHello()
-    {
-        System.out.println("Hello World!");
-    }
-}
---
+++ Java Codes from Basic to Advanced/C31_Class_Fields_and_Methods.java
@@ -0,0 +1,40 @@
+public class C31_Class_Fields_and_Methods
+{
+    String name;
+    int age;
+    char gender;
+
+    // These are Methods.
+    void setData(String n, int a, char g)
+    {
+        name = n;
+        age = a;
+        gender = g;
+    }
+
+    void displayData()
+    {
+        System.out.println("----------------------------");
+        System.out.println("Name = " + name);
+        System.out.println("Age = " + age);
+        System.out.println("Gender = " + gender);
+    }
+
+    public static void main(String[] args)
+    {
+        C31_Class_Fields_and_Methods obj = new C31_Class_Fields_and_Methods();
+        obj.setData("Atharva", 20, 'M');
+        obj.displayData();
+
+        obj.setData("Nishant", 23, 'F');
+        obj.displayData();
+        System.out.println("----------------------------");
+        System.out.println("End of the Program");
+        System.out.println("----------------------------");
+    }
+}
+
+// It Contains Hardcore Values.
+// We can also take Input from the User using Scanner Class.
+// But it is not a good practice to take Input from the User in this way.
+// It is better to take Input from the User in the 'Main()' method.

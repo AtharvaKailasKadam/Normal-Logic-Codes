602b2d96b1af323d3b9145fe9b097ce64890737b
Your Name <atharvakailaskadam1809@gmail.com>
2025-08-27 19:28:12 +0530

    Done and Dusted

--- Java Codes from Basic to Advanced/C31_String_is_a_Palindrome.java
+++ Java Codes from Basic to Advanced/C31_String_is_a_Palindrome.java
@@ -1,4 +1,41 @@
+import java.util.Scanner;
+
 public class C31_String_is_a_Palindrome
 {
-    
+    public void Palindrome(String str)
+    {
+        String s1 = str;
+        str = str.toLowerCase();
+        int len = str.length();
+        String rev = "";
+
+        for(int i = len - 1; i >= 0; i--)
+        {
+            rev = rev + str.charAt(i);
+        }
+
+        if(str.equals(rev))
+        {
+            System.out.println("The Given String i.e. "+ s1 + " is a Palindrome String");
+        }
+        else
+        {
+            System.out.println("The Given String i.e. "+ s1 + " is Not a Palindrome String");
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println("Palindrome Check Program");
+
+        System.out.println("Enter the String to be Checked..!");
+        Scanner scanner = new Scanner(System.in);
+
+        String string = scanner.nextLine();
+        scanner.close();
+
+        C31_String_is_a_Palindrome SIP = new C31_String_is_a_Palindrome();
+        SIP.Palindrome(string);
+
+    }
 }

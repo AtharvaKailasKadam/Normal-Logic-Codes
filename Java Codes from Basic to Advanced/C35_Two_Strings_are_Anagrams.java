2d2bb00121739712948850e698c728406233033f
Your Name <atharvakailaskadam1809@gmail.com>
2025-08-28 09:50:33 +0530

    Done and Dusted

---
+++ Java Codes from Basic to Advanced/C35_Two_Strings_are_Anagrams.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class C35_Two_Strings_are_Anagrams
+{
+    public boolean Anagram(String str1, String str2)
+    {
+        String string1 = str1.toLowerCase();
+        String string2 = str1.toLowerCase();
+        int len = str1.length();
+
+        char char1, char2;
+        boolean flag;
+
+        if(str1.length() != str2.length())
+        {
+            System.out.println("The Two Strings i.e. "+ str1 + " and " + str2 + " are not ANAGRAMS...!");
+            return false;
+        }
+        else
+        {
+            for(int i = 0; i < len; i++ )
+            {
+                flag = false;
+                for(int j = 0; j < len; j++)
+                {
+                    if(string1.charAt(i) == string2.charAt(j))
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+                if(flag == false)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println("Program to Check Anagrams");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the First String : ");
+        String str1 = scanner.nextLine();
+
+        System.out.println("Enter the Second String : ");
+        String str2 = scanner.nextLine();
+
+        scanner.close();
+
+        C35_Two_Strings_are_Anagrams A = new C35_Two_Strings_are_Anagrams();
+        boolean b = A.Anagram(str1, str2);
+        System.out.println("----------------------------");
+        System.out.println(b);
+        System.out.println("----------------------------");
+
+    }
+}

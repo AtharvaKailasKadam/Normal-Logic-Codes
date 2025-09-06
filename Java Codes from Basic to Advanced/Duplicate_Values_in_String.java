29228bb8a560e03b02aba0dbe0575181b3dd7bff
Your Name <atharvakailaskadam1809@gmail.com>
2025-09-06 11:00:03 +0530

    Done with Duplicate String Values

--- Java Codes from Basic to Advanced/Duplicate_Values_in_String.java
+++ Java Codes from Basic to Advanced/Duplicate_Values_in_String.java
@@ -5,23 +5,31 @@ public class Duplicate_Values_in_String
     public void DuplicateValuesInString(String str)
     {
         str = str.toLowerCase();
+        String checked = "";
+
         for(int i = 0; i < str.length(); i++)
         {
+            if(checked.indexOf(str.charAt(i)) != -1)
+            {
+                continue;
+            }
             char ch1 = str.charAt(i);
-            for(int j = i +1; j < str.length(); j++)
+            int count = 1;
+            for(int j = i + 1; j < str.length(); j++)
             {
                 char ch2 = str.charAt(j);
                 if(ch1 == ch2)
                 {
-                    char Duplicate = ch1;
-                    System.out.println("The Duplicate Values are : "+ch1);
-                    int count = 0;
-                    count ++;
-                    System.out.println("For : "+ count + " times.");
+                    count = count + 1;
                 }
             }
+            if(count > 1)
+            {
+                System.out.println("The Duplicate Character is : " + ch1);
+                System.out.println("For : "+ count + " times.");
+            }
+            checked = checked + ch1;
         }
-
     }
 
     public static void main(String args[])

dac4d3eaf11b20d3e9dbb5456b1639206a3a6cd9
Your Name <atharvakailaskadam1809@gmail.com>
2025-10-28 21:50:22 +0530

    Done and Dusted

---
+++ ARRAYS/A12_Move_All_Zeros_At_The_End.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class A12_Move_All_Zeros_At_The_End
+{
+    public void MoveAllZerosAtTheEnd(int [] arr)
+    {
+        int n = arr.length;
+        int count = 0;
+        for(int i = 0; i < n; i++)
+        {
+            if(arr[i] != 0)
+            {
+                arr[count] = arr[i];
+                count++;
+            }
+        }
+        while(count < n)
+        {
+            arr[count] = 0;
+            count++;
+        }
+        System.out.println("The Elements in the Array After Moving the Elements at the End : ");
+        for(int i = 0; i < arr.length; i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+    public static void main(String args[])
+    {
+        System.out.println("Move All Zeros At The End : ");
+
+        System.out.println("Enter the Size of the Array : ");
+
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+
+        System.out.println("Enter the Elements in the Array : ");
+        int [] arr = new int [size];
+        for(int i = 0; i < size; i++)
+        {
+            arr[i] = scanner.nextInt();
+        }
+
+        scanner.close();
+
+        A12_Move_All_Zeros_At_The_End obj = new A12_Move_All_Zeros_At_The_End();
+        obj.MoveAllZerosAtTheEnd(arr);
+    }
+}
